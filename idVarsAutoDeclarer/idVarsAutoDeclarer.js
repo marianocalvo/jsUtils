0cf571bd2b1b951f9f1aadbadaa7afc88547dad1
jairo <kidandcat@gmail.com>
2015-12-11 02:02:56 +0100

    not using eval now

--- idVarsAutoDeclarer/idVarsAutoDeclarer.js
+++ idVarsAutoDeclarer/idVarsAutoDeclarer.js
@@ -1,4 +1,5 @@
-function getStringForEvalIds(node) {
+function getStringForEvalIds(node, scope) {
+    var _scope = scope || window;
     var aIds = [];
     var getAllIdsOfNode = function (node) {
         if (node.hasAttribute("id"))
@@ -12,9 +13,14 @@ function getStringForEvalIds(node) {
     getAllIdsOfNode(node);
     var stringForEval = "";
     for (var i in aIds) {
-        stringForEval += 'var ' + aIds[i] + ' = document.getElementById("' + aIds[i] + '");';
+      _scope[aIds[i]] = document.getElementById(aIds[i]);
+      //stringForEval += 'var ' + aIds[i] + ' = document.getElementById("' + aIds[i] + '");';
     }
+
     return stringForEval;
 }
 
 // Call eval(getStringForEvalIds(node)); from wichever scope you want the variables to work
+//
+// Call getStringForEvalIds(node) or if you dont want to use the global(window) scope, pass an object to add the variable there:
+// getStringForEvalIds(exampleElement, myObject);   myObject.exampleElement;
